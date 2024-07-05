3f752d57e69957f53fc94e0bea15ce07934124cb
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2024-07-05 19:28:36 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS code as newer version
+
+// alert("3 + 3") // error in node js, not browser
+
+console.log(3 + 
+    3
+) // code readibillity should be high
+
+console.log("abhay")
+
+
+let name = "abhay"
+let age = 20
+let isloggedIn = false
+
+// number => 2 to power 53
+// bigimt
+// string => ""
+// Boolean => true/false
+// null => standakone value
+// undefined => 
+// Symbol => unique
+
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof null); // object (in terminal) o/p
+console.log(typeof undefined); // undefined in terminal
