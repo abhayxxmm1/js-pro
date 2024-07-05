cacf1f90d11ff755773ad965d6af872ea4327931
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2024-07-05 19:30:20 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOpresation.js
@@ -0,0 +1,27 @@
+let score = "abhay"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);  // NaN NotaNumber
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+let isLoggedIn = "abhay"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "abhay" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
