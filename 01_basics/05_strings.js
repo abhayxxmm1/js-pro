c6fa71f2ca2d6e5c552473bef3be3c91fcc95c46
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2024-07-07 09:06:36 +0000

    String(`bacticks[syntaxMustFollow]`) String.methods(KeyValuePair gameName[1], length, toUpper/lowerCase, charAt(5), indexOf('i'), substring, slice, trim, replace('%94', '-'), includes, split('-'){into array}

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,40 @@
+const name = "abhay"
+const repoCount = 10
+
+// console.log(name + repoCount + "Value"); // this is Old
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // StringInterpolation
+
+
+const gameName = new String("abhi-hc-com")
+
+// console.log(gameName[1]); // here accesing keyValue Pair
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('i'));
+
+
+const newString = gameName.substring(0,4) // 4 is not included here
+console.log(newString);
+
+const anotherString = gameName.slice(-4,4)
+console.log(anotherString);
+
+const newStringOne = "    abhay   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://abhay.com/abhay%94ghongade"
+
+console.log(url.replace('%94', '-'));
+
+console.log(url.includes('abhay')); 
+console.log(url.includes('yumi')); 
+
+
+console.log(gameName.split('-'));  // changing into array format
