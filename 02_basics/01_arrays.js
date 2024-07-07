f51cf9d81d3859f926a8bcd1dd2d32822420bbe4
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2024-07-07 18:59:51 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,32 @@
+// Array javascript mnd
+// const numbers = [1, 2, 3, 4 ] 🏃‍♂️
+// numbers 🏃‍♂️
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHerros = ["ironman", "thanos"]
+
+const myArr2 = new Array(1,2,3,4) // automatically ads square bracket
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9)); // gives ans 1 or 0 and imp ⭐ boolean datatype
+// console.log(myArr.indexOf(9)); // => -1 [not present in array then]
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr); // => string
+
+
+
+
+// slice, splice 💻
+
