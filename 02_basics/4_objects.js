51756b3e1e6fa3da2ddebf101c66f3e3ffdf0ab6
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2024-07-09 10:40:56 +0000

    ➡️⬆️

--- 02_basics/4_objects.js
+++ 02_basics/4_objects.js
@@ -1,6 +1,8 @@
 // const tinderUser = new Object() //singelton Object💻
  const tinderUser = {} // Non-singelton Object
 
+// left ➡️ + up ⬆️ teminal copy
+
 tinderUser.id = "123abc" // unique id देदेते है
 tinderUser.name = "samm"
 tinderUser.isLoggedIn = false
