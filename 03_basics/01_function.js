f6db6e61420ffad953f65bcabab4febb717d906f
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2024-07-12 16:47:36 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,27 @@
+// code ko package mai band kar diya hai
+function sayMyName() { // scope, defination
+    console.log("A");
+    console.log("B");
+    console.log("H");
+    console.log("A");
+    console.log("Y");
+}
+
+// sayMyName'is_refrence' ()'is_execution'
+// ⬆️⬆️⬆️useful while react or onclick & dom Manipulation
+// sayMyName()
+
+/*
+
+function addTwoNumbers(number1, number2){ //(number1, number2)Parameters
+    console.log(number1 + number2);
+}
+addTwoNumbers(3, "a") // (3, 4)Arrgument
+addTwoNumbers(3, "4")
+addTwoNumbers(3, null)
+// problem ⬇️
+const result = addTwoNumbers(3,5)
+console.log("Result: ",result); // =>Undefined
+
+*/
+
