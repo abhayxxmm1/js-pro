cd52c10f3a06297977660a63c0c2000e476fa7a4
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2024-07-13 16:35:08 +0000

    . . .rest operator

---
+++ 03_basics/011_function.js
@@ -0,0 +1,10 @@
+/* 🔸shoping cart in e-commerse website
+🔸problem!! user keeps on adding multiple no. of items 
+🔸so, u dont know how many no. of argument/items are going to come 
+🔸then how to create parameters in function */
+
+function calculateCartPrice(/* val1, val2, ...num1imp */ ...num1 /* ...is a rest/spread operator specified upon useCase */) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 500, 400, 2000));
