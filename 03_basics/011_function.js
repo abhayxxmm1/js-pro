b032725297e8035c3b7ab7aac04b7fbba2c24581
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2024-07-13 16:51:19 +0000

    object pass in func

--- 03_basics/011_function.js
+++ 03_basics/011_function.js
@@ -3,8 +3,25 @@
 🔸so, u dont know how many no. of argument/items are going to come 
 🔸then how to create parameters in function */
 
-function calculateCartPrice(/* val1, val2, ...num1imp */ ...num1 /* ...is a rest/spread operator specified upon useCase */) {
+function calculateCartPrice(/* val1, val2, */ ...num1 /* ...is a rest/spread operator specified upon useCase */) { // also cant use two operator at time error⚠️
     return num1
 }
 
 // console.log(calculateCartPrice(200, 500, 400, 2000));
+
+
+/* 🔸object pass Usecase*/
+const user = {
+    username: "abhay",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Usernmae is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user) /* functionCall */
+/* handleObject({ // or like this also
+    username: "sam",
+    price: 99
+}) */
