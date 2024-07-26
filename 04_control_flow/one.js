7eb729eda59261f637b7f00e87a4596210646e06
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2024-07-26 15:19:26 +0000

    else if && (multiple) || (multiple)

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,7 +1,7 @@
 // if 
 const isUserLoggedIn = true
 const temprature = 5
-/* 
+/* //1️⃣ 
 if (temprature === 52) {
     console.log("less than 50");
 } else {
@@ -11,4 +11,52 @@ if (temprature === 52) {
 console.log("execute"); 
 */
 
-// <, >, <=, >=, ==, !=, ===, !==
+// <, >, <=, >=, ==, !=, ===, !== 
+
+
+/* //2️⃣
+const score = 200
+
+if (score > 100) {
+    let power = "fly"  // var is a global scope
+    console.log(`User power: ${power}`);
+}
+
+console.log(`User power: ${power}`);
+*/
+
+
+/* //3️⃣
+const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2"); // 🔸2️⃣Implicite scope in 03_basics/03_Arrow.js ❌❌❌but do not write such a code
+
+
+if (balance < 500) {
+    console.log("less than 500");
+
+}   else if (balance < 750) {
+    console.log("less than 750");
+
+} else if (balance < 900) {
+    console.log("less than 900");
+
+} else {  // their is condition for else
+    console.log("less than 1200");
+
+}
+*/
+
+  //4️⃣
+const isUserLoggedIn2 = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (isUserLoggedIn2 && debitCard && 2==3 /* 2==2 */) {
+    console.log("allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail /* multiple or can be used || */) {
+    console.log("User logged in");
+}
