d62f17c50f8a0da5842be8fb1f05f47d248b0cf7
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2024-08-02 03:41:20 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,8 @@
+// for 
+
+for (let index = 0/* is statement main loop kabhi aega hi nahi baadmaii */; index < 10/*↙️ cond checking hoti hai ⬅️⬅️*/;/*⬅️⬅️*/ index++) {
+    const element = index;
+    console.log(element);//↙️database ko call karna hai/ print karana hai ye sab kam hoga 
+    
+/* just block scope khatam hone se pahele index ki value ko badha deta hai ↗️*/}
+
