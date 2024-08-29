48819b715ad00e98909fc61fe356bddf08b63ab0
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2024-08-29 06:01:13 +0000

    reduce example

--- 05_iterations/08_reduce.js
+++ 05_iterations/08_reduce.js
@@ -1,11 +1,39 @@
-// shopping cart addition of total money
+// shopping cart addition of total price
 
 const myNums = [1, 2, 3]
 
-const myTotal = myNums.reduce(function (acc, currval) {
-    console.log(`acc: ${acc} and currval ${currval}`);
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval ${currval}`);
     
-    return acc + currval
-}, 0)
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc + curr, 0)
 
 console.log(myTotal);
+
+
+// example 
+
+const shoppingCart = [
+    {
+        itemname: "lana",
+        price: 2999
+    },
+    {
+        itemname: "mia",
+        price: 999
+    },
+    {
+        itemname: "lexi",
+        price: 5999
+    },
+    {
+        itemname: "alyx",
+        price: 12999
+    },
+]
+
+const total = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(total);
