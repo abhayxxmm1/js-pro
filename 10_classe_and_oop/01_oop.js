7ff521141045c77a1a88345f0a84bb64b6f49a8d
Abhay <ghongadeabhay60@gmail.com>
2024-12-30 14:27:14 +0530

    Refactor user object and add User constructor function for instance creation need to revise this again

--- 10_classe_and_oop/01_oop.js
+++ 10_classe_and_oop/01_oop.js
@@ -1,15 +1,14 @@
 const user = {
-    username: "abhay",
-    loginCount: "8",
-    signedIn: true,
+  username: "abhay",
+  loginCount: "8",
+  signedIn: true,
 
-    getUserDetails: function () {
-        // console.log("got user details from database");
-        // console.log(`username: ${this.username}`);
-        console.log(this);
-        
-    }
-}
+  getUserDetails: function () {
+    // console.log("got user details from database");
+    // console.log(`username: ${this.username}`);
+    console.log(this);
+  },
+};
 
 console.log(user.username);
 // console.log(user.getUserDetails());
@@ -17,5 +16,27 @@ console.log(user.username);
 
 // const promiseOne = new/*constructor function*/ Promise()
 // const date = new/*constructor function*/ Date()
+//  this constructor function allow,s एक ही object literal से आप  multiple instance बना सको
+// new is used for making new context
 
-// 21
+function User(username, loginCount, isLoggedIn) {
+  this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greetings = function () {
+        console.log(`Welcome ${this.username}`);
+        
+    }
+
+    return this // write verbos code (implicitly return define hota hai)
+}
+
+const userOne = User("hitesh", 12, true)
+const userTwo = User("bumrah", 11, false) // values overright kar di
+
+console.log(userOne.constructor);
+// console.log(userTwo);
+
+
+// instanceof on mdn : don,t depend on spoonfeeding
