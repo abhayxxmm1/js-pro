fb4f58137213bb1f8a220ca0928c89964df4e441
Abhay <ghongadeabhay60@gmail.com>
2025-03-02 16:00:01 +0530

    how to inject own function using prototype with properties applied - Object & array hierachy, also INHERITANCE // kisi aur ki properties ko kaise access krr sakte hai yahi prototypal inheritance hai

--- 10_classe_and_oop/prototype.js
+++ 10_classe_and_oop/prototype.js
@@ -5,7 +5,7 @@
 // console.log(myName.truelength);
 
 
-let myHeros = ("thor", "spiderman")
+let myHeros = ["thor", "spiderman"]
 
 let heroPower = {
     thor : "hammer",
@@ -22,5 +22,51 @@ Object.prototype.abhay = function(){
     
 }
 
+Array.prototype.heyAbhay = function () {
+    console.log(`abhay says helllo`);
+    
+}
+
+
 // heroPower.abhay()
 myHeros.abhay()
+myHeros.heyAbhay()
+// heroPower.heyabhay()
+
+// ÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷ INHERITANCE ÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable : false
+}
+
+const TASupport = {
+    makeAssingment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport // taking refrence or borowing properties by sing this syntax/keyword
+}
+
+/* we can take acess from out side also */
+Teacher.__proto__ = User 
+// teacher bhi user ki sari properties access krr sakta hai
+// kisi aur ki properties ko kaise access krr sakte hai yahi prototypal inheritance hai
+
+/* modern syntax */
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "CanISmellUrPantiess      "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`); 
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
