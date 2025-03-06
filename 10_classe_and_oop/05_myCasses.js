67d5328a394c13f1e902f688db06253c05ab6b5c
Abhay <ghongadeabhay60@gmail.com>
2025-03-07 00:30:54 +0530

    behind the seen कैसे कैसे होता है

--- 10_classe_and_oop/05_myCasses.js
+++ 10_classe_and_oop/05_myCasses.js
@@ -2,22 +2,49 @@
 
 // Syntactic sugar refers to certain language features in programming that make the code easier to read or write
 
-class User { // class अब एक key-word है JS मैं
-    constructor(username, email, password){
-        /* for context =>*/ this.username = username ;
-        this.email = email;
-        this.password = password
-    }
+
+// class User { // class अब एक key-word है JS मैं
+//     constructor(username, email, password){
+//         /* for context =>*/ this.username = username ;
+//         this.email = email;
+//         this.password = password
+//     }
     
-    // password CLEAR_TEXT_FORMAT मैं तो नहीं रखोगे means unencrypted
-    encryptPassword(){ // है तो ये भी function ही पर class के अंदर है तो method बोलने लग गये 
-        return /* variable ले लेते है ${}*/`${this.password}abc`
+//     // password CLEAR_TEXT_FORMAT मैं तो नहीं रखोगे means unencrypted
 
-    }
+//     encryptPassword(){ // है तो ये भी function ही पर class के अंदर है तो method बोलने लग गये 
+//         return /* variable ले लेते है ${}*/`${this.password}abc`
 
+//     } /* और भी method add केआर देते है इसमें  */
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+
+// //  इससे अब एक user बना लेते है chai ⏚
+// const chai = new User("chai", "chai@gmail.com","123")
+
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUsername());
+
+
+
+// BEHIND_THE_SEEN
+
+function User(username, email, password){
+    this.username = username ;
+    this.email = email;
+    this.password = password
 }
 
-//  इससे अब एक user बना लेते है chai ⏚
-const chai = new User("chai", "chai@gmail.com","123")
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
 
-console.log(chai.encryptPassword());
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
