c91abbb6f4defbd2c91b41f9e07e2e1ab452fa0b
Abhay <ghongadeabhay60@gmail.com>
2025-03-07 00:35:49 +0530

    about : class constructor

--- 10_classe_and_oop/05_myCasses.js
+++ 10_classe_and_oop/05_myCasses.js
@@ -3,29 +3,29 @@
 // Syntactic sugar refers to certain language features in programming that make the code easier to read or write
 
 
-// class User { // class अब एक key-word है JS मैं
-//     constructor(username, email, password){
-//         /* for context =>*/ this.username = username ;
-//         this.email = email;
-//         this.password = password
-//     }
+class User { // class अब एक key-word है JS मैं
+    constructor(username, email, password){
+        /* for context =>*/ this.username = username ;
+        this.email = email;
+        this.password = password
+    }
     
-//     // password CLEAR_TEXT_FORMAT मैं तो नहीं रखोगे means unencrypted
+    // password CLEAR_TEXT_FORMAT मैं तो नहीं रखोगे means unencrypted
 
-//     encryptPassword(){ // है तो ये भी function ही पर class के अंदर है तो method बोलने लग गये 
-//         return /* variable ले लेते है ${}*/`${this.password}abc`
+    encryptPassword(){ // है तो ये भी function ही पर class के अंदर है तो method बोलने लग गये 
+        return /* variable ले लेते है ${}*/`${this.password}abc`
 
-//     } /* और भी method add केआर देते है इसमें  */
-//     changeUsername(){
-//         return `${this.username.toUpperCase()}`
-//     }
-// }
+    } /* और भी method add केआर देते है इसमें  */
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
 
-// //  इससे अब एक user बना लेते है chai ⏚
-// const chai = new User("chai", "chai@gmail.com","123")
+//  इससे अब एक user बना लेते है chai ⏚
+const chai = new User("chai", "chai@gmail.com","123")
 
-// console.log(chai.encryptPassword());
-// console.log(chai.changeUsername());
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
 
 
 
@@ -48,3 +48,5 @@ const tea = new User("tea", "tea@gmail.com", "123")
 
 console.log(tea.encryptPassword());
 console.log(tea.changeUsername());
+
+// this is about class constructor
