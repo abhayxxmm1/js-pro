8e83b5159c1c800aa7870330ad2f797f6a3f6b1e
Abhay <ghongadeabhay60@gmail.com>
2025-03-09 18:57:58 +0530

    Math.PI value change kuu nhi ho rahi (getOwnPropertyDescriptor) & Object.definePropwerty to change {writable: false, enumerable: false, configurable: true }

---
+++ 10_classe_and_oop/09_mathpi.js
@@ -0,0 +1,41 @@
+/*
+ object prototype की properties inherit है और object की ख़ुद की भी properties है
+जैसे की constructurs new_keyword class सब यही से चाल रहा है */
+
+const discriptor = Object.getOwnPropertyDescriptor(Math, "PI"); 
+/*
+  PI जो है property का name है और 3.14 उसकी value है || PI की value single quote double quote किसी मैं भी लिख सकते है */
+
+// console.log(discriptor); // check this
+
+/* 
+console.log(Math.PI);
+Math.PI = 5
+console.log(Math.PI); // value change नहीं हो रही है 🍑 किऊ नहीं हो रही 
+ */
+
+const chai = {
+  name: "jinger",
+  price: 243,
+  isAvailable: true,
+
+  orderChai: function () {
+    console.log(`chai nhi bani /* code faat gaya */`);
+  },
+};
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name',{
+    // writable: false,
+    enumerable: false,
+    // configurable: true
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) { // objects के लिए easy रहता है forof
+  if (typeof value !== 'function') { //for handling code
+    
+      console.log(`${key} : ${value}`);
+}
+}
